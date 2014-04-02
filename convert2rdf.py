01bcf83caef59ec693aa496d1e734a8ee7677697
rtroncy <raphael.troncy@eurecom.fr>
2014-04-02 08:40:09 +0200

    Minor fix in the py conversion code

--- convert2rdf.py
+++ convert2rdf.py
@@ -15,8 +15,7 @@ f = open('results/dbpedia.json')
 data = json.load(f) 
 f.close()
 
-#print data
-## namespaces definition
+# namespaces definition
 FOAF = Namespace("http://xmlns.com/foaf/0.1/")
 RDF = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
@@ -26,17 +25,13 @@ XSD = Namespace("http://www.w3.org/2001/XMLSchema#")
 FRESNEL = Namespace("http://www.w3.org/2004/09/fresnel#")
 DBPEDIAOWL = rdflib.Namespace("http://dbpedia.org/ontology/") 
 PROV = Namespace("http://www.w3.org/ns/prov#") 
-
 EIP = Namespace("http://data.eurecom.fr/def/eip#") # future eip ontology
 GOO_RESOURCE = rdflib.Namespace("http://data.eurecom.fr/gp/")
 
-
-#Graph prepation
+# Graph prepation
 gResults = rdflib.ConjunctiveGraph()
 
-
 # ----- converting-----
-
 for summary in data:
 	for cat in data.keys():
 		category = cat
@@ -50,37 +45,21 @@ for summary in data:
 		gResults.add((curi, FRESNEL["purpose"], FRESNEL["defaultLens"]))
 		gResults.add((curi, FRESNEL["classLensDomain"], DBPEDIAOWL[category])) #adding the categories
 		gResults.add((curi, FRESNEL["group"], guri))
-		gResults.add((curi, PROV["wasDerivedFrom"], URIRef("http://www.google.com/insidesearch/features/search/knowledge.html")))
-    
+		gResults.add((curi, PROV["wasDerivedFrom"], URIRef("http://www.google.com/insidesearch/features/search/knowledge.html")))    
 		partd ="("
 		partf = ")"  
 		space = " "
-		
-		
 		summ =data[category]["summary"] # taking the summary elements
-		#infoless =data[category]["infoboxless"]
+		#infoless = data[category]["infoboxless"]
 		for prop in summ.keys(): #looping through the properties
 			proplower = prop.lower()
 			if space in proplower:
 				st = proplower.strip(). split(space)
-
 				propstr = "".join(t for t in st)
-				#print propstr
+				# print propstr
 			gResults.add((curi, FRESNEL["showProperties"], EIP[propstr] ))
 
-		#
-
-		
-
-
-
-
-
-
-
-
 # ----- OUTPUT FILE -----
-
 
 print "Length of gResults : " + str(len(gResults))
 print "Nb of lens : " + str(len(list(gResults.triples((None, RDF["type"], FRESNEL["Lens"])))))
@@ -88,4 +67,3 @@ print "Nb of lens : " + str(len(list(gResults.triples((None, RDF["type"], FRESNE
 outfile = open("results.n3", "w")
 outfile.write(gResults.serialize(format='n3'))
 outfile.close()
-
