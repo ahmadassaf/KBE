3ca9d6f5532fc57808d65d015e236daec567dd1a
Ahmad Assaf <me@ahmadassaf.com>
2014-09-25 00:37:57 +0200

    removing un-needed express library

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-var express             = require('express');
 var knowledge_extractor = require('./KBE');
-var app                 = express();
 
 KBE = new knowledge_extractor();
 KBE.start();
 
-console.log('Knolwedge Scrapper Server Started at localhost:8080');
-exports = module.exports = app;
+console.log('Knowledge Scrapper Server Started at localhost:8080');
